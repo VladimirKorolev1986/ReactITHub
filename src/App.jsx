4acb676f0a6e5d5e94a1bed69e1914cbdf43e848
VladimirKorolev <theing1986@gmail.com>
2025-01-05 18:20:50 +0300

    CardCategories

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import Button from "./components/UI/Button/Button";
+import CardCategories from "./components/UI/CardCategories/CardCategories";
 
 function App() {
-  return <Button />;
-}
 
+  return <div className="app">
+    <Button />
+    <CardCategories />
+  </div>
+
+}
 export default App;
--- src/components/UI/CardCategories/CardCategories.jsx
+++ src/components/UI/CardCategories/CardCategories.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./CardCategories.css";
 
-function CardCategories({ children }) {
-  return (
-    <div className="card-categories">
-      <img
-        className="card-categories__img"
-        src="/public/cards/Fertilizer.png"
-        alt=""
-      />
-      <h2 className="card-categories__paragraph">{children}</h2>
-    </div>
-  );
+function CardCategories() {
+	return (
+		<div className="card-categories">
+			<img
+				className="card-categories__img"
+				src="../../public/cards/Fertilizer.png"
+				alt=""
+			/>
+			<h2 className="card-categories__paragraph">Text</h2>
+		</div>
+	);
 }
 
 export default CardCategories;
